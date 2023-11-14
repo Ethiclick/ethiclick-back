13b0877c7b8d62e9a488516868b3ef533a8c3e77
Emmanuelle WINKLER <e.winkler@geomatika.fr>
2023-11-14 12:41:23 +0100

    Test ajour clé étrangère - Professionnels

--- app/Models/Professionnel.ts
+++ app/Models/Professionnel.ts
@@ -1,8 +1,13 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import CategorieOne from './CategorieOne'
+import CategorieTwo from './CategorieTwo'
+import CategorieThree from './CategorieThree'
+import PriceRange from './PriceRange'
+import Abonnement from './Abonnement'
 
 export default class Professionnel extends BaseModel {
-  // public static table = 'professionnels';
 
   @column({ isPrimary: true })
   public id: number
@@ -34,11 +39,32 @@ export default class Professionnel extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
-    // TODO: ajouter les clé étrangère
-    // id_cat1
-    // id_cat2
-    // id_cat3
-    // id_prix
-    // id_abo
-    // id_user
+
+    // ** Clé étrangère
+
+    @belongsTo(() => User)
+    public id_user: BelongsTo<typeof User>
+
+    @belongsTo(() => CategorieOne)
+    public id_cat1: BelongsTo<typeof CategorieOne>
+
+    @belongsTo(() => CategorieTwo)
+    public id_cat2: BelongsTo<typeof CategorieTwo>
+
+    @belongsTo(() => CategorieThree)
+    public id_cat3: BelongsTo<typeof CategorieThree>
+
+    @belongsTo(() => PriceRange)
+    public id_priceRange: BelongsTo<typeof PriceRange>
+
+    @belongsTo(() => Abonnement)
+    public id_abo: BelongsTo<typeof Abonnement>
+    // @hasOne(() => User, {
+    //   // pour tester lequel sera le nom de la colonne qui contiendra la clé étragnère
+    //   // ! namingStrategie Adonis -> snakeCase
+    //   serializeAs: 'serializeAs',
+    //   localKey: 'localKey',
+    //   foreignKey: 'user_pro'
+    // })
+    // public user: HasOne<typeof User>
 }
