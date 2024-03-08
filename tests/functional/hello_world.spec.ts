053b64b3dc189d8c8063dfe810910a08cd1a52a0
EmmanuelleWinkler <111369621+EmmanuelleWinkler@users.noreply.github.com>
2024-03-08 14:15:11 +0100

    Test

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,8 +1,15 @@
 import { test } from '@japa/runner'
 
-test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
 
-  response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+test("say test", async () => {
+  console.log("test");
+
 })
+
+// test('display welcome page', async ({ client }) => {
+//   // const response = await client.get('/')
+
+//   // response.assertStatus(200)
+//   // response.assertBodyContains({ hello: 'world' })
+//   console.log("test");
+// })
