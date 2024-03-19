a594163ef46a58a42c6546ac324d9a2b5b0a3cc5
EmmanuelleWinkler <111369621+EmmanuelleWinkler@users.noreply.github.com>
2024-03-19 13:09:42 +0100

    <fix> Correction update pros

--- app/Controllers/Http/ProfessionnelsController.ts
+++ app/Controllers/Http/ProfessionnelsController.ts
@@ -7,7 +7,8 @@ export default class ProfessionnelsController {
   // si id on renvoie selon l'identifiant
   // si on à le level tout les pros de ce level
   // Sinon on renvoie tout les pros
-  public async get () {
+  public async get ()
+  {
     const pro = await Professionnel.all();
     return pro
   }
@@ -30,34 +31,37 @@ export default class ProfessionnelsController {
       let regexUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w\-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
       // validation des champs
       const validations = schema.create({
-        id: schema.number(),
-        // siret : Est ce qu'on peut mettre à jour un siret?
-        adresse: schema.string(),
-        city: schema.string(),
-        postal_code: schema.number(),
-        website: schema.string([rules.regex(regexUrl)]),
-        acc_card: schema.boolean(),
-        photos: schema.string(),
+        id: schema.number.optional(),
+        adresse: schema.string.optional(),
+        city: schema.string.optional(),
+        postal_code: schema.number.optional(),
+        website: schema.string.optional([rules.regex(regexUrl)]),
+        acc_card: schema.boolean.optional(),
+        photos: schema.string.optional(),
       })
 
       // Validation des données de la requête
       const data = await request.validate({ schema: validations })
+
+      if (!data.id) {
+        return response.status(400).send({ message: 'Veuillez renseigner l\'idenfitiant de l\'utilisateur pour le mettre à jour' });
+      }
       // Récupération de l'utilisateur à modifier
       const pro = await Professionnel.findOrFail(data.id)
 
       // Mettre à jour
-      pro.adresse = data.adresse
-      pro.city = data.city
-      pro.postal_code = data.postal_code
-      pro.website = data.website
-      pro.acc_card = data.acc_card
-      pro.photos = data.photos
+      if (data.adresse) pro.adresse = data.adresse;
+      if (data.city) pro.city = data.city;
+      if (data.postal_code) pro.postal_code = data.postal_code;
+      if (data.website) pro.website = data.website;
+      if (data.acc_card) pro.acc_card = data.acc_card
+      if (data.photos) pro.photos = data.photos
 
       await pro.save()
 
       return response.status(200).send({ message: 'Profil mis à jour avec succès' })
     } catch (error) {
-      return response.send(error.messages);
+      return response.send({ "message" : error.message});
     }
   }
 
