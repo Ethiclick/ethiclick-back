2b7e9df85cd24a571b8217947dd31c22aa5dbb52
EmmanuelleWinkler <111369621+EmmanuelleWinkler@users.noreply.github.com>
2024-07-08 13:20:57 +0200

    maj categories

--- database/seeders/CategorieOne.ts
+++ database/seeders/CategorieOne.ts
@@ -10,45 +10,53 @@ export default class CategoriesOneSeeder extends BaseSeeder {
         color: 'rgba(11, 192, 236, 1)',
       },
       {
+        libelle: 'Restaurants, bars, cafés',
+        color: 'rgba(30, 225, 83, 1)',
+      },
+      {
         libelle: 'Bien-être',
         color: 'rgba(249, 219, 103, 1)',
-      },
-      {
-        libelle: 'Mode',
-        color: 'rgba(249, 103, 206, 1)',
-      },
-      {
-        libelle: 'Loisirs',
-        color: 'rgba(46, 138, 138, 1)',
       },
       {
         libelle: 'Finances',
         color: 'rgba(130, 214, 88, 1)',
       },
       {
-        libelle: 'Faune & flore',
-        color: 'rgba(30, 225, 83, 1)',
+        libelle: 'Loisirs',
+        color: 'rgba(46, 138, 138, 1)',
       },
       {
         libelle: 'Habitat',
         color: 'rgba(40, 215, 214, 1)',
       },
       {
-        libelle: 'Technologie',
-        color: 'rgba(132, 49, 190, 1)',
+        libelle: 'Mode',
+        color: 'rgba(249, 103, 206, 1)',
       },
       {
-        libelle: 'Transport',
-        color: 'rgba(236, 55, 11, 1)',
+        libelle: 'Services',
+        color: 'rgba(249, 103, 206, 1)',
       },
-      {
-        libelle: 'Écolieux',
-        color: 'rgba(177, 90, 75, 1)',
-      },
-      {
-        libelle: 'Nettoyage',
-        color: 'rgba(168, 11, 236, 1)',
-      },
+      // {
+      //   libelle: 'Faune & flore',
+      //   color: 'rgba(30, 225, 83, 1)',
+      // },
+      // {
+      //   libelle: 'Technologie',
+      //   color: 'rgba(132, 49, 190, 1)',
+      // },
+      // {
+      //   libelle: 'Transport',
+      //   color: 'rgba(236, 55, 11, 1)',
+      // },
+      // {
+      //   libelle: 'Écolieux',
+      //   color: 'rgba(177, 90, 75, 1)',
+      // },
+      // {
+      //   libelle: 'Nettoyage',
+      //   color: 'rgba(168, 11, 236, 1)',
+      // },
     ])
   }
 }
